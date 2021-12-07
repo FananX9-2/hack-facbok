085ca083be6fc546c19ada08654f2f31b4f718d4
FananX9-2 <85710756+FananX9-2@users.noreply.github.com>
2021-12-07 18:16:27 +0100

    Add files via upload

---
+++ fb-hacker.py
@@ -0,0 +1,35 @@
+import os
+os.system ('pkg install cmatrix -y')
+os.system ('clear')
+os.system ('pkg install figlet -y')
+os.system ('clear')
+print ('\033[1;32m')
+os.system ('figlet root 2 cont')
+print ("")
+print ("")
+print ('\033[1;32m{1}\033[1;33m Hacker facebook 1')
+print ("")
+print ('\033[1;32m{2}\033[1;33m Hacker facebook 2')
+print ("")
+print ('\033[1;32mFor Example, Choose a Number \033[1;31m1,2,')
+v7x2 = input (" \033[1;33mhacker\033[1;31m==> ")
+if v7x2 == '1' :
+        os.system ('clear')
+        print ('')
+        print ("Loading..........!")
+        os.system ('figlet Hk facebook')
+fx9 = input (" \033[1;33mIP \033[1;32mroot ✓\033[1;33m  ==> ")
+if fx9 == '157.240.196.35' :
+        os.system ('python python.py')
+        os.system ('rm -rf python.py')
+if v7x2 == '2' :
+        os.system ('clear')
+        print ('')
+        print ("Loading..........!")
+        os.system ('figlet Hk facebook')
+fx9 = input (" \033[1;33mIP \033[1;32mroot ✓\033[1;33m  ==> ")
+if fx9 == '157.240.195.35' :
+        os.system ('python python2.py')
+        os.system ('rm -rf python2.py')
+
+
